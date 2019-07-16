a94382fa66e42b43818a373ac7bdb63329d5ba26
aaron <kenzzw@163.com>
2019-07-16 16:16:36 +0800

    Add: add model class EmployeeController

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeesController.java
@@ -0,0 +1,21 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+
+    @GetMapping
+    public List<Employee>getAllEmployees(){
+        return Employee.createTestEmployees();
+    }
+
+    @PostMapping
+    public void addEmployees(@RequestBody Employee employee){
+        System.out.println(employee.getName());
+    }
+}
